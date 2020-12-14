c32077fad5c3781949859a7a447fd46e4643e428
Pedro Miguez <pedrovieiramiguez@upskill.pt>
2020-12-14 17:00:11 +0000

    Update Inserção Jornal + fixes CriaçaoTabelasJornal

--- SQL/Jornal/CriacaoTabelasJornal.sql
+++ SQL/Jornal/CriacaoTabelasJornal.sql
@@ -111,14 +111,22 @@ CREATE TABLE Pessoa (
     nome            VARCHAR(50)
         CONSTRAINT nn_Pessoa_Nome NOT NULL,
     dataNascimento  DATE
-        CONSTRAINT ck_Pessoa_DataNascimento CHECK ( dataNascimento >= TO_DATE('01-01-1940', 'mm-dd-yyyy') );
+        CONSTRAINT ck_Pessoa_DataNascimento CHECK ( dataNascimento >= TO_DATE('01-01-1940', 'mm-dd-yyyy') )
+);
 
-CREATE TABLE EdicaoJornal ( nrEdicao INTEGER, idJornal
-        INTEGER, precoBase FLOAT
-    CONSTRAINT ck_EdicaoJornal_precoBase CHECK(precoBase >= 0), precoVenda FLOAT, idJornalAnterior INTEGER, nrEdicaoAnterior INTEGER,
-    CONSTRAINT pk_EdicaoJornal_nrEdicao_idJornal PRIMARY KEY(nrEdicao,
-                                                                                                                                idJornal),
-CONSTRAINT ck_EdicaoJornal_precoVenda CHECK(precoVenda > precoBase) );
+CREATE TABLE EdicaoJornal (
+    nrEdicao          INTEGER,
+    idJornal          INTEGER,
+    precoBase         FLOAT
+        CONSTRAINT ck_EdicaoJornal_precoBase CHECK ( precoBase >= 0 ),
+    precoVenda        FLOAT,
+    idJornalAnterior  INTEGER,
+    nrEdicaoAnterior  INTEGER,
+    CONSTRAINT pk_EdicaoJornal_nrEdicao_idJornal PRIMARY KEY ( nrEdicao,
+                                                               idJornal ),
+    CONSTRAINT ck_EdicaoJornal_precoVenda CHECK ( precoVenda > precoBase )
+);
+
 CREATE TABLE PapelJornalistaNoticia (
     idPapel     INTEGER PRIMARY KEY,
     designacao  VARCHAR(30)
--- SQL/Jornal/InsercaoJornal.sql
+++ SQL/Jornal/InsercaoJornal.sql
@@ -197,14 +197,540 @@ INSERT INTO EdicaoJornal (
     1
 );
 
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    1,
+    1,
+    1
+);
 
-UPDATE Cliente
-SET
-    nrCliente = 0002,
-set
-    idPessoa = 002,
-WHERE
-    idPessoa = 001;
-DELETE FROM Cliente
-WHERE
-    idPessoa = 001;
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    2,
+    1,
+    1
+);
+
+
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    1,
+    2,
+    1
+);
+
+
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    2,
+    2,
+    1
+);
+
+
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    1,
+    2,
+    1
+);
+
+
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    2,
+    2,
+    1
+);
+
+
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    1,
+    1,
+    2
+);
+
+
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    2,
+    1,
+    2
+);
+
+
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    1,
+    2,
+    2
+);
+
+
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    2,
+    2,
+    2
+);
+
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    1,
+    1,
+    3
+);
+
+
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    2,
+    1,
+    3
+);
+
+
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    1,
+    2,
+    3
+);
+
+
+INSERT INTO Noticia (
+    idNoticia,
+    nrEdicao,
+    idJornal
+) VALUES (
+    2,
+    2,
+    3
+);
+
+INSERT INTO Pessoa (
+    idPessoa,
+    nome,
+    dataNascimento
+) VALUES (
+    1,
+    'Ricardo Luis',
+    TO_DATE('25/01/1965', 'dd/mm/yyyy')
+);
+
+INSERT INTO Pessoa (
+    idPessoa,
+    nome,
+    dataNascimento
+) VALUES (
+    2,
+    'Maria Luz',
+    TO_DATE('11/06/1993', 'dd/mm/yyyy')
+);
+
+INSERT INTO Pessoa (
+    idPessoa,
+    nome,
+    dataNascimento
+) VALUES (
+    3,
+    'Rogério Luz',
+    TO_DATE('12/06/1993', 'dd/mm/yyyy')
+);
+
+INSERT INTO Pessoa (
+    idPessoa,
+    nome,
+    dataNascimento
+) VALUES (
+    4,
+    'Rui Sousa',
+    TO_DATE('30/11/1987', 'dd/mm/yyyy')
+);
+
+INSERT INTO Jornalista (
+    nrIdCivil,
+    idPessoa
+) VALUES (
+    1111,
+    1
+);
+
+
+INSERT INTO Jornalista (
+    nrIdCivil,
+    idPessoa
+) VALUES (
+    1112,
+    2
+);
+
+INSERT INTO Jornalista (
+    nrIdCivil,
+    idPessoa
+) VALUES (
+    1113,
+    3
+);
+
+INSERT INTO Jornalista (
+    nrIdCivil,
+    idPessoa
+) VALUES (
+    1114,
+    4
+);
+
+INSERT INTO PapelJornalistaNoticia (
+    idPapel,
+    designacao
+) VALUES (
+    1,
+    'autor'
+);
+
+INSERT INTO PapelJornalistaNoticia (
+    idPapel,
+    designacao
+) VALUES (
+    2,
+    'co-autor'
+);
+
+INSERT INTO PapelJornalistaNoticia (
+    idPapel,
+    designacao
+) VALUES (
+    3,
+    'revisor'
+);
+
+INSERT INTO Cliente (
+    nrCliente,
+    idPessoa
+) VALUES (
+    1,
+    1
+);
+
+INSERT INTO Cliente (
+    nrCliente,
+    idPessoa
+) VALUES (
+    2,
+    2
+);
+
+INSERT INTO Cliente (
+    nrCliente,
+    idPessoa
+) VALUES (
+    3,
+    3
+);
+
+INSERT INTO Cliente (
+    nrCliente,
+    idPessoa
+) VALUES (
+    4,
+    4
+);
+
+INSERT INTO PontoVenda (
+    idPontoVenda,
+    idMorada
+) VALUES (
+    1,
+    1
+);
+
+INSERT INTO PontoVenda (
+    idPontoVenda,
+    idMorada
+) VALUES (
+    2,
+    2
+);
+
+INSERT INTO NoticiaJornalista (
+    idNoticia,
+    nrEdicao,
+    idJornal,
+    nrIdCivil,
+    idPapel
+) VALUES (
+    1,
+    1,
+    1,
+    1111,
+    1
+);
+
+INSERT INTO NoticiaJornalista (
+    idNoticia,
+    nrEdicao,
+    idJornal,
+    nrIdCivil,
+    idPapel
+) VALUES (
+    1,
+    1,
+    1,
+    1112,
+    2
+);
+
+INSERT INTO NoticiaJornalista (
+    idNoticia,
+    nrEdicao,
+    idJornal,
+    nrIdCivil,
+    idPapel
+) VALUES (
+    1,
+    1,
+    1,
+    1113,
+    3
+);
+
+INSERT INTO NoticiaJornalista (
+    idNoticia,
+    nrEdicao,
+    idJornal,
+    nrIdCivil,
+    idPapel
+) VALUES (
+    2,
+    2,
+    3,
+    1112,
+    3
+);
+
+INSERT INTO NoticiaJornalista (
+    idNoticia,
+    nrEdicao,
+    idJornal,
+    nrIdCivil,
+    idPapel
+) VALUES (
+    2,
+    2,
+    3,
+    1111,
+    2
+);
+
+INSERT INTO NoticiaJornalista (
+    idNoticia,
+    nrEdicao,
+    idJornal,
+    nrIdCivil,
+    idPapel
+) VALUES (
+    2,
+    2,
+    3,
+    1113,
+    1
+);
+
+
+INSERT INTO NoticiaJornalista (
+    idNoticia,
+    nrEdicao,
+    idJornal,
+    nrIdCivil,
+    idPapel
+) VALUES (
+    1,
+    1,
+    1,
+    1111,
+    1
+);
+
+INSERT INTO NoticiaJornalista (
+    idNoticia,
+    nrEdicao,
+    idJornal,
+    nrIdCivil,
+    idPapel
+) VALUES (
+    1,
+    1,
+    1,
+    1112,
+    2
+);
+
+INSERT INTO Compra (
+    idCompra,
+    nrEdicao,
+    idJornal,
+    idPontoVenda,
+    nrCliente,
+    quantidade
+) VALUES (
+    1,
+    1,
+    1,
+    1,
+    1,
+    5
+);
+
+
+INSERT INTO Compra (
+    idCompra,
+    nrEdicao,
+    idJornal,
+    idPontoVenda,
+    nrCliente,
+    quantidade
+) VALUES (
+    7,
+    1,
+    1,
+    1,
+    2,
+    3
+);
+
+INSERT INTO Compra (
+    idCompra,
+    nrEdicao,
+    idJornal,
+    idPontoVenda,
+    nrCliente,
+    quantidade
+) VALUES (
+    2,
+    1,
+    1,
+    1,
+    3,
+    2
+);
+
+INSERT INTO Compra (
+    idCompra,
+    nrEdicao,
+    idJornal,
+    idPontoVenda,
+    nrCliente,
+    quantidade
+) VALUES (
+    3,
+    2,
+    3,
+    1,
+    4,
+    10
+);
+
+INSERT INTO Compra (
+    idCompra,
+    nrEdicao,
+    idJornal,
+    idPontoVenda,
+    nrCliente,
+    quantidade
+) VALUES (
+    4,
+    1,
+    2,
+    2,
+    3,
+    1
+);
+
+INSERT INTO Compra (
+    idCompra,
+    nrEdicao,
+    idJornal,
+    idPontoVenda,
+    nrCliente,
+    quantidade
+) VALUES (
+    5,
+    2,
+    1,
+    2,
+    4,
+    4
+);
+
+INSERT INTO Compra (
+    idCompra,
+    nrEdicao,
+    idJornal,
+    idPontoVenda,
+    nrCliente,
+    quantidade
+) VALUES (
+    6,
+    1,
+    3,
+    1,
+    1,
+    2
+);
+
+select * from Compra;
+commit;
+
+delete from NoticiaJornalista where (nrIdCivil = 1111 AND idPapel = 3);
+
+--UPDATE Cliente
+--SET
+--    nrCliente = 0002,
+--set
+--    idPessoa = 002,
+--WHERE
+--    idPessoa = 001;
+--DELETE FROM Cliente
+--WHERE
+--    idPessoa = 001;
